e932336cde55cd0d7942183c828a7e9887627029
devi9113624552 <dpraidola@gmail.com>
2024-11-05 10:57:13 +0530

    updated categories for news

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,20 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [news, setNews] = useState([]);
+  const [category, setCategory] = useState("all");
+
+  const selectHandler = (e) => {
+    setCategory(e.target.value);
+    console.log(e.target.value);
+  };
+
   const fetchNews = async () => {
     try {
       const response = await axios.get(
-        "https://news-api-test.vercel.app/news?category=all"
+        `https://news-api-test.vercel.app/news?category=${category}`
       );
+      console.log(`https://news-api-test.vercel.app/news?category=${category}`);
+
       const newsData = response.data.data;
       setNews(newsData);
       console.log(newsData);
@@ -23,34 +32,61 @@ function App() {
     // Set an interval to fetch news every 5 minutes
     const intervalId = setInterval(() => {
       fetchNews();
-    }, 300000); // 5 minutes in milliseconds
+    }, 60000); // 5 minutes in milliseconds
 
     // Cleanup function to clear the interval on unmount
     return () => clearInterval(intervalId);
-  }, []); // Empty dependency array to run once on mount
+  }, [category]); // Empty dependency array to run once on mount
   return (
-    news.length > 0 &&
-    news.map((news) => {
-      return (
-        <div
-          key={news.id}
-          className="mx-10 my-5 border shadow-lg rounded-md h-[120vh] md:h-[100vh] justify-center items-start px-4 py-3"
+    <>
+      <div className="flex justify-between items-center m-10">
+        <select
+          name="category"
+          id="category"
+          className="border outline-none px-5 shadow-sm py-2"
+          onChange={selectHandler}
+          value={category}
         >
-          <div className="space-y-4 my-5">
-            <h3 className="text-2xl font-semibold">{news.author}</h3>
-            <h4 className="text-md text-justify">{news.content}</h4>
-          </div>
-          <h5 className="text-md my-2 font-bold">
-            {news.date}&&&{news.time}
-          </h5>
-          <img
-            src={news.imageUrl}
-            className="w-[100%] h-[50vh] object-cover rounded-lg"
-            alt={news.title}
-          />
-        </div>
-      );
-    })
+          <option value="all">All</option>
+          <option value="sports">Sports</option>
+          <option value="national">national</option>
+          <option value="world">world</option>
+          <option value="politics">politics</option>
+          <option value="technology">technology</option>
+          <option value="business">Business</option>
+          <option value="entertainment">entertainment</option>
+          <option value="miscellaneous">miscellaneous</option>
+          <option value="startup">Startup</option>
+          <option value="hatke">hatke</option>
+          <option value="science">Science</option>
+          <option value="automobile">automobile</option>
+        </select>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-3 sm:grid-cols-2 ">
+        {news.length > 0 &&
+          news.map((news) => {
+            return (
+              <div
+                key={news.id}
+                className="mx-10 my-5 border shadow-lg rounded-md h-[120vh] md:h-[100vh] justify-center items-start px-4 py-3"
+              >
+                <div className="space-y-4 my-5">
+                  <h3 className="text-2xl font-semibold">{news.author}</h3>
+                  <h4 className="text-md text-justify">{news.content}</h4>
+                </div>
+                <h5 className="text-md my-2 font-bold">
+                  {news.date}&&&{news.time}
+                </h5>
+                <img
+                  src={news.imageUrl}
+                  className="w-[100%] h-[50vh] object-cover rounded-lg"
+                  alt={news.title}
+                />
+              </div>
+            );
+          })}
+      </div>
+    </>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,6 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <div className="grid grid-cols-1 md:grid-cols-3 sm:grid-cols-2 ">
-      <App />
-    </div>
+    <App />
   </StrictMode>
 );
